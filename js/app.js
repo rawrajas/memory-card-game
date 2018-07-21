1cac33a7666d1dd310ee7abbcd6535584275b3a0
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2018-07-21 02:32:17 -0500

    feat: Add Stopwatch function

--- js/app.js
+++ js/app.js
@@ -101,7 +101,83 @@ let tilesFlipped = 0;
 
 let tilesMatched = [];
 
+//Declare a counter for amount of moves
 
+let numMoves = 0;
+
+//Stopwatch function from https://codepen.io/_Billy_Brown/pen/dbJeh
+
+class Stopwatch {
+    constructor(display, results) {
+        this.running = false;
+        this.display = display;
+        this.results = results;
+        this.laps = [];
+        this.reset();
+        this.print(this.times);
+    }
+
+    reset() {
+        this.times = [ 0, 0, 0 ];
+    }
+
+    start() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+    }
+
+    stop() {
+        this.running = false;
+        this.time = null;
+    }
+
+    restart() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+        this.reset();
+    }
+
+    step(timestamp) {
+        if (!this.running) return;
+        this.calculate(timestamp);
+        this.time = timestamp;
+        this.print();
+        requestAnimationFrame(this.step.bind(this));
+    }
+
+    calculate(timestamp) {
+        var diff = timestamp - this.time;
+        // Hundredths of a second are 100 ms
+        this.times[2] += diff / 10;
+        // Seconds are 100 hundredths of a second
+        if (this.times[2] >= 100) {
+            this.times[1] += 1;
+            this.times[2] -= 100;
+        }
+        // Minutes are 60 seconds
+        if (this.times[1] >= 60) {
+            this.times[0] += 1;
+            this.times[1] -= 60;
+        }
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `\
+        ${pad0(times[0], 2)}:\
+        ${pad0(times[1], 2)}:\
+        ${pad0(Math.floor(times[2]), 2)}`;
+    }
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
