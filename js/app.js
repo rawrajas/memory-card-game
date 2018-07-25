83ca4511b2ac3f81cc9627244e44573a485f00f4
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2018-07-25 02:33:23 -0500

    style: Add semicolons to end of 'arrow' functions

--- js/app.js
+++ js/app.js
@@ -201,8 +201,7 @@ let stopwatch = new Stopwatch(
 
 //Create a function for a new game
 
-newGame = () =>
-{
+newGame = () => {
     //Reset the stopwatch and moves
     stopwatch.reset();
     numMoves =0;
@@ -267,7 +266,6 @@ clickFunction = (element) =>
         }
         //If the tilesMatched array is equal to the amount of cards array, win the game
         if (tilesMatched.length == deck.cards.length) {
-            console.log('You win!');
             stopwatch.stop();
             //Display the final moves result on the win page
             document.getElementsByClassName('movesResult')[0].innerHTML = numMoves;
@@ -294,20 +292,7 @@ clickFunction = (element) =>
             }
         ,600);
         }
-    }
+    };
 };
 
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 newGame();
-
